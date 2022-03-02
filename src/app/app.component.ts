0f2cfcd0a977b9d51bc4a38f8ea9d01acec7a373
Felipe Lobato <felipelobato99@poli.ufrj.br>
2022-03-02 20:19:07 -0300

    Criando repositório no github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'front-end';
+  title = 'Bem vindo ao Curso de Front-end com Angular';
 }
